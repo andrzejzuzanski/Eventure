8ac9f241be00680365c2811c43da7412ff2f8b88
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2025-07-14 20:07:12 +0200

    Add project files.

---
+++ Eventure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eventure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
