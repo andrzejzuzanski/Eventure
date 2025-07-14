dd3ebf2b683a8b25ef5b5f3ba6aebbad4a64d800
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2025-07-14 21:12:05 +0200

    Feat: Added Event controller and Create/Index views

---
+++ Eventure/Controllers/EventsController.cs
@@ -0,0 +1,68 @@
+﻿using Eventure.Data;
+using Eventure.Models;
+using Eventure.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventure.Controllers
+{
+    [Authorize]
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EventsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Events 
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Events
+                .Include(e => e.Organizer)
+                .OrderBy(e => e.StartDateTime)
+                .ToListAsync();
+
+            return View(events);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EventCreateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                var newEvent = new Event
+                {
+                    Title = vm.Title,
+                    Description = vm.Description,
+                    StartDateTime = vm.StartDateTime,
+                    EndDateTime = vm.EndDateTime,
+                    Location = vm.Location,
+                    MaxParticipants = vm.MaxParticipants,
+                    OrganizerId = user.Id
+                };
+
+                _context.Add(newEvent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+    }
+}
---
+++ Eventure/Views/Events/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eventure.Views.Events
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
