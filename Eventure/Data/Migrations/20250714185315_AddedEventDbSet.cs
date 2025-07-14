22adab4a49cca77889c94ef70ca10314b9acb6a3
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2025-07-14 21:09:37 +0200

    Fix: Added Event DbSet

---
+++ Eventure/Data/Migrations/20250714185315_AddedEventDbSet.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEventDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Event_AspNetUsers_OrganizerId",
+                table: "Event");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Event",
+                table: "Event");
+
+            migrationBuilder.RenameTable(
+                name: "Event",
+                newName: "Events");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Event_OrganizerId",
+                table: "Events",
+                newName: "IX_Events_OrganizerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Events",
+                table: "Events",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId",
+                table: "Events",
+                column: "OrganizerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId",
+                table: "Events");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Events",
+                table: "Events");
+
+            migrationBuilder.RenameTable(
+                name: "Events",
+                newName: "Event");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_OrganizerId",
+                table: "Event",
+                newName: "IX_Event_OrganizerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Event",
+                table: "Event",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Event_AspNetUsers_OrganizerId",
+                table: "Event",
+                column: "OrganizerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
